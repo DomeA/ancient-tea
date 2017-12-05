152d08dabd0aa843151d66c9c1b58e20aab685ad
DomeA <domeastudio@gmail.com>
2017-12-05 19:53:49 +0800

    描述

--- servers/micro-service/surveying/src/main/java/com/domeastudio/mappingo/servers/microservice/surveying/domain/mongodb/services/impl/UserServiceImpl.java
+++ servers/micro-service/surveying/src/main/java/com/domeastudio/mappingo/servers/microservice/surveying/domain/mongodb/services/impl/UserServiceImpl.java
@@ -1,20 +1,19 @@
 package com.domeastudio.mappingo.servers.microservice.surveying.domain.mongodb.services.impl;
 
-import com.domeastudio.mappingo.servers.microservice.surveying.domain.mongodb.pojo.MUsersEntity;
-import com.domeastudio.mappingo.servers.microservice.surveying.domain.mongodb.repository.MUsersRepository;
+import com.domeastudio.mappingo.servers.microservice.surveying.domain.mongodb.pojo.BpmnFileEntity;
+import com.domeastudio.mappingo.servers.microservice.surveying.domain.mongodb.repository.BpmnFileRepository;
 import com.domeastudio.mappingo.servers.microservice.surveying.domain.mongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
-    MUsersRepository mUsersRepository;
+    BpmnFileRepository mUsersRepository;
     @Override
     public void saveUser() {
-        MUsersEntity m=new MUsersEntity();
+        BpmnFileEntity m=new BpmnFileEntity();
 
         m.setName("domea1234");
         m.setId("dddddsq121234234324");
@@ -23,7 +22,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public String findByName(String name) {
-        MUsersEntity u= mUsersRepository.findByName(name);
+        BpmnFileEntity u= mUsersRepository.findByName(name);
         return u.toString();
     }
 }
