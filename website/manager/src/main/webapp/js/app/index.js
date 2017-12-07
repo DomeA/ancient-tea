8bf5e6e3c2c873774ba8fe778b34a6a751434673
DomeA <domeastudio@gmail.com>
2017-12-07 20:22:20 +0800

    surveying 格式化

--- website/manager/src/main/webapp/js/app/index.js
+++ website/manager/src/main/webapp/js/app/index.js
@@ -45,8 +45,7 @@
                 $('.page-container form').submit(function(){
                     var username = $(this).find('.username').val();
                     var password = $(this).find('.password').val();
-                    var clientId = $(this).find('.clientId').val();
-                    if(clientId == '') {
+                    if(username == '') {
                         $(this).find('.error').fadeOut('fast', function(){
                             $(this).css('top', '27px');
                         });
@@ -55,7 +54,7 @@
                         });
                         return false;
                     }
-                    if(username == '') {
+                    if(password == '') {
                         $(this).find('.error').fadeOut('fast', function(){
                             $(this).css('top', '96px');
                         });
@@ -64,17 +63,8 @@
                         });
                         return false;
                     }
-                    if(password == '') {
-                        $(this).find('.error').fadeOut('fast', function(){
-                            $(this).css('top', '165px');
-                        });
-                        $(this).find('.error').fadeIn('fast', function(){
-                            $(this).parent().find('.clientId').focus();
-                        });
-                        return false;
-                    }
                 });
-                $('.page-container form .username, .page-container form .password,.page-container form .clientId').keyup(function(){
+                $('.page-container form .username, .page-container form .password').keyup(function(){
                     $(this).parent().find('.error').fadeOut('fast');
                 });
 
